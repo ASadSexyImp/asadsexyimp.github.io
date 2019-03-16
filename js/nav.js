9d1fcd0bc55fce13369d0644fa09db7aa938da48
Masakazu Nishimura <masakazuplus@gmail.com>
2019-03-16 21:27:48 +0900

    smoothscroll

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@
            $(window).scroll(function(){
          //セクション２から３の間はこれ
         if ($(window).scrollTop() > $('#about').offset().top &&　$(window).scrollTop() < $('#skill').offset().top){
-             $("a.about").css("color", "#1f1f1f");
+             $(".about").css("color", "#1f1f1f");
         }//セクション３より進んだらこれ
              else if($(window).scrollTop() > $('#skill').offset().top){
              $("nav ul li").css("color", "#ff0000");
